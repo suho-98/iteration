5afdcb12a56c58b54d60d4d1cc309ff4f9018dcd
suho's <qusql98@naver.com>
2025-06-04 13:34:52 +0900

    commit2

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,10 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        int number = 5;
-        int number2 = 6;
-
         for (int i = 1; i <= 9; i++) {
-            System.out.println(number + " x " + i + " = " + (number * i));
-
-
+            //<--
+            for (int j = 1; j <= 9; j ++)
+                System.out.println(  i + "x" +  + j);
         }
-
-
-            for (int i = 1; i <= 9; i++) {
-                System.out.println(number2 + " x " + i + " = " + (number2 * i)); }
-        }
+        }   //<--
 }
